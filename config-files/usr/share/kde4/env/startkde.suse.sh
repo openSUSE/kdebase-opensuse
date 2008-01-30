e2f1404afcf038851c702fafeafe1ccf4903f7ec
Stephan Binner <stbinner@suse.de>
2008-01-30 14:14:34 +0000

    add default desktop icons

---
+++ config-files/usr/share/kde4/env/startkde.suse.sh
@@ -0,0 +1,128 @@
+echo "+++ startkde.suse.sh running" 
+
+#
+# do we run in a prelinked system ?
+#
+if test -f /etc/sysconfig/prelink; then
+. /etc/sysconfig/prelink
+  if test "$USE_PRELINK" = "yes" ; then
+     KDE_IS_PRELINKED=1
+     export KDE_IS_PRELINKED
+  else
+     unset KDE_IS_PRELINKED
+  fi
+fi
+
+# 
+# workaround SaX/SUSE bug that doesn't setup a proper keyboard map
+#
+
+if [ -f /usr/share/hotkey-setup/hotkey-setup.xmodmap ]; then
+  xmodmap /usr/share/hotkey-setup/hotkey-setup.xmodmap
+fi
+
+#
+# Do we have a special Gtk theming for our Qt widget theme ?
+#
+if [ ! -e $HOME/.no-qtrc-to-gtkrc-mapping ]; then
+
+  SUSE_VERSION="`cat /etc/SuSE-release | sed -n 's/VERSION = //p'`"
+
+    # Defaults for openSUSE 10.3
+    GTK2_SYSCONFDIR=/etc
+    GTK2_DATADIR=/usr/share/themes/
+
+  # use general gtk-qt-engine
+  if [ -e "$GTK2_DATADIR/Qt/gtk-2.0/gtkrc" ] ; then
+    GTK2_THEME_RC="$GTK2_DATADIR/Qt/gtk-2.0/gtkrc"
+  else
+    # use QtCurve engine
+    if [ -e "$GTK2_DATADIR/QtCurve/gtk-2.0/gtkrc" ] ; then
+       GTK2_THEME_RC="$GTK2_DATADIR/QtCurve/gtk-2.0/gtkrc"
+    fi
+  fi
+
+  # GTK2
+  # NOTE: ~/.gtkrc-2.0-kde is added later (in latest KDE only)
+  if [ "$GTK2_RC_FILES" ]; then
+    export GTK2_RC_FILES="$GTK2_RC_FILES:$GTK2_THEME_RC:$HOME/.gtkrc-2.0-qtengine:$HOME/.gtkrc-2.0"
+  else
+    export GTK2_RC_FILES="$GTK2_SYSCONFDIR/gtk-2.0/gtkrc:$GTK2_THEME_RC:$HOME/.gtkrc-2.0-qtengine:$HOME/.gtkrc-2.0"
+  fi
+
+fi
+
+if [ -r /etc/sysconfig/windowmanager ]; then
+  # Do the user want the SuSE theme ?
+  source /etc/sysconfig/windowmanager
+
+  # Should we really enable FAM support for KDE ?
+  export USE_FAM="$KDE_USE_FAM"
+
+  # Disable IPv6 ?
+  if [ "$KDE_USE_IPV6" = "no" ]; then
+     export KDE_NO_IPV6=1
+  fi
+  # Disable IDN ?
+  if [ "$KDE_USE_IDN" = "no" ]; then
+     export KDE_NO_IDN=1
+  fi
+
+else
+  if [ -r /etc/rc.config ]; then
+    # Do the user want the SuSE theme ?
+    INSTALL_DESKTOP_EXTENSIONS=`bash -c "source /etc/rc.config && echo \\$INSTALL_DESKTOP_EXTENSIONS"`
+
+    # Should we really enable FAM support for KDE ?
+    USE_FAM=`bash -c "source /etc/rc.config && echo \\$KDE_USE_FAM"`
+    export USE_FAM
+  fi
+fi
+
+#
+# create SuSE defaults
+#
+if [ ! -e "$HOME/.skel/kdebase4.110" ]; then
+    mkdir -p $HOME/Desktop
+
+    if [ ! -e "$HOME/Desktop/trash.desktop" -a -e "/usr/share/kde4/config/SuSE/default/trash.desktop" ]; then
+          cp /usr/share/kde4/config/SuSE/default/trash.desktop $HOME/Desktop/
+    fi
+
+    if [ ! -e "$HOME/Desktop/MozillaFirefox.desktop" -a -e "/usr/share/kde4/config/SuSE/default/MozillaFirefox.desktop" ]; then
+          cp /usr/share/kde4/config/SuSE/default/MozillaFirefox.desktop $HOME/Desktop/
+    fi
+
+    if [ ! -e "$HOME/Desktop/Office.desktop" -a -e "/usr/share/kde4/config/SuSE/default/Office.desktop" ]; then
+          cp /usr/share/kde4/config/SuSE/default/Office.desktop $HOME/Desktop/
+    fi
+
+    if [ ! -e "$HOME/Desktop/SuSE.desktop" -a -e "/usr/share/kde4/config/SuSE/default/SuSE.desktop" ]; then
+          cp /usr/share/kde4/config/SuSE/default/SuSE.desktop $HOME/Desktop/
+    fi
+
+    if [ ! -e "$HOME/Desktop/Support.desktop" -a -e "/usr/share/kde4/config/SuSE/default/Support.desktop" ]; then
+          cp /usr/share/kde4/config/SuSE/default/Support.desktop $HOME/Desktop/
+    fi
+
+    mkdir -p $HOME/.skel/
+    touch $HOME/.skel/kdebase4.110
+fi
+
+# check if any rpms have been (un)installed since ksycoca
+# had been built, if yes, trigger ksycoca rebuild immediatelly
+# instead of delayed
+
+kdehome=$HOME/.kde4
+test -n "$KDEHOME" && kdehome=`echo "$KDEHOME"|sed "s,^~/,$HOME/,"`
+host=$HOSTNAME
+test -n "$XAUTHLOCALHOSTNAME" && host=$XAUTHLOCALHOSTNAME
+ksycoca="$kdehome/cache-$host/ksycoca"
+
+if test -f "$ksycoca"; then
+    if test -f /var/lib/rpm/Packages; then
+	if test /var/lib/rpm/Packages -nt "$ksycoca"; then
+	    rm -f "$ksycoca"
+	fi
+    fi
+fi
