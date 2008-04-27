84905170234fdcfa20d954377712079b626d5378
Dirk Mueller <dmueller@suse.de>
2008-04-27 20:13:59 +0000

    add the translations to the package

--- po/fill_po.sh
+++ po/fill_po.sh
@@ -1,23 +1,25 @@
+#!/bin/bash
+# vim: sw=4 et
 
-SUBDIRS = kfiledialog kio_sysinfo krpmview kryptomedia SUSEgreeter suseplugger susetranslations
+SUBDIRS="kfiledialog kio_sysinfo krpmview SUSEgreeter suseplugger kde4-openSUSE"
 
-update:
-	for i in $(SUBDIRS); do \
-		test -d $$i && sed "s:%PACKAGE%:$$i:" < Makefile.am.template > $$i/Makefile.am; \
-	done
+rm -f CMakeLists.txt
+echo "find_package(Gettext REQUIRED)" >> CMakeLists.txt
 
-fill_pos:
-	LANGS=`cd $$MY_LCN_CHECKOUT && ls -1d ?? ??_??` ;\
-	echo $$LANGS; \
-	for i in $(SUBDIRS); do \
-		for lang in $$LANGS; do \
-		  echo -n "copying $$lang/po/$$i.$$lang.po... " ;\
-		  if test -f  "$$MY_LCN_CHECKOUT/$$lang/po/$$i.$$lang.po"; then \
-			cp $$MY_LCN_CHECKOUT/$$lang/po/$$i.$$lang.po $$i/$$lang.po ;\
-			echo "done" ; \
-		  else \
-			echo "missing" ;\
-		  fi ;\
-		done ;\
-	done
-
+LANGS=`cd $MY_LCN_CHECKOUT && ls -1d ?? ??_??`
+echo $LANGS
+for lang in $LANGS; do
+    PO_LIST=""
+    for i in $SUBDIRS; do
+        echo -n "copying $lang/po/$i.$lang.po... "
+        if test -f  "$MY_LCN_CHECKOUT/$lang/po/$i.$lang.po"; then
+            mkdir -p $lang
+            cp -p $MY_LCN_CHECKOUT/$lang/po/$i.$lang.po $lang/$i.po
+            PO_LIST="$PO_LIST $lang/$i.po"
+            echo "done"
+        else
+            echo "missing"
+        fi
+    done
+    echo "gettext_process_po_files($lang ALL INSTALL_DESTINATION \${LOCALE_INSTALL_DIR} $PO_LIST )" >> CMakeLists.txt
+done
