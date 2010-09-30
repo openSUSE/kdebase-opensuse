a352696d8b0ad95c2861d595e8cf7f051422919e
Luboš Luňák <l.lunak@suse.cz>
2010-09-30 14:48:15 +0200

    slightly update the translations howto

--- totranslate/extract_all_strings.sh
+++ totranslate/extract_all_strings.sh
@@ -7,10 +7,10 @@ OSC_PATH=~/wrk/osc
 
 for s in $(<spec_list); do
 
-    if ! test -f $OSC_PATH/KDE:KDE4:Factory:Desktop/$s ; then
+    if ! test -f $OSC_PATH/KDE:Distro:Factory/$s ; then
         echo "$s does not exist.. "
         exit 1
     fi
-    ./extract_strings.sh $OSC_PATH/KDE:KDE4:Factory:Desktop/$s
+    ./extract_strings.sh $OSC_PATH/KDE:Distro:Factory/$s
 done
 msgcat -s *.po > kde4-openSUSE.pot
